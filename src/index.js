2fe88ed808c18e06cafa7e8d123bab48630554eb
Hanania RAZAFINARIVO <razafi.hanania@gmail.com>
2025-04-04 14:15:41 +0300

    Display only OTP code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@
 const env = require("./env");
 const generate = require("./otp-generator");
 
-const parameters = env.getGenerationParameters();
-parameters.clockOffset = null
-console.log(parameters);
-console.log(generate(parameters));
+console.log(generate(env.getGenerationParameters()));
